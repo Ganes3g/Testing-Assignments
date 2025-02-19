cb30afb401c521253decf981154830e869ee3e10
Ganes3g <157215403+Ganes3g@users.noreply.github.com>
2025-02-19 21:10:05 +0530

    Add files via upload
    Control statements

---
+++ day3/FindingEvenNumber.java
@@ -0,0 +1,18 @@
+package weekOne.day3;
+
+public class FindingEvenNumber {
+	
+	public static void main(String[] args) {
+		int a=10;
+		if (a%2==0) {
+			System.out.println("The value is Even number "+ a);
+		}
+		else if (a%2==1) {
+			System.out.println("The value is odd number "+ a);
+		}
+		else {
+			System.out.println("Not a valid number");
+		}
+	}
+
+}
